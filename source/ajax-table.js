464aa786c9eab02155a9b660f6538dd0ed000e2c
Nha Hoang <nhaht94@gmail.com>
2018-03-11 19:20:09 +0700

    nhaht update

--- source/ajax-table.js
+++ source/ajax-table.js
@@ -2,7 +2,7 @@
   AT_Init($('#at-place-here'),
     {
       columns : [
-        { title: 'Name',     property: 'name',     sortable: false },
+        { title: 'Name',     property: 'name',     sortable: true },
         { title: 'Address',  property: 'address',  sortable: false },
         { title: 'Email',    property: 'email',    sortable: false },
         { title: 'Phone',    property: 'phone',    sortable: false }
@@ -15,6 +15,43 @@
         pageIndexPath   : 'pageIndex',
         searchInputPath : 'searchInput',
         viewCountPath   : 'viewCount',
+        sortPath        : 'sort',
+
+        // output
+        dataPath        : 'data',                     // data | data.list....
+        countPath       : 'count',                    // count | total | ...
+        statusPath      : 'status',                   // count | total | ...
+
+        // config ajax call
+        method          : 'POST',
+        dataType        : 'JSON',
+        headers         : {},
+        data            : {},
+        url             : 'http://localhost:3100/api/test/example_data'
+      },
+      // callback on complete
+      complete: function() {
+        console.log('#at-place-here DONE!');
+      }
+    });
+
+  AT_Init($('#at-place-here2'),
+    {
+      columns : [
+        { title: 'Name',     property: 'name',     sortable: true },
+        { title: 'Address',  property: 'address',  sortable: false },
+        { title: 'Email',    property: 'email',    sortable: false },
+        { title: 'Phone',    property: 'phone',    sortable: false }
+      ],
+      // show index column
+      index   : false,
+      api     : {
+        // config path data params
+        // input
+        pageIndexPath   : 'pageIndex',
+        searchInputPath : 'searchInput',
+        viewCountPath   : 'viewCount',
+        sortPath        : 'sort',
 
         // output
         dataPath        : 'data',                     // data | data.list....
@@ -34,21 +71,18 @@
 
 var AT_Config;
 var AT_CurrentSelector;
-var AT_IsSearching;
-var AT_CurrentSearchInput;
-var AT_IsChangedShowCount;
-var AT_CurrentShowCount;
 
 function AT_Init(selector, config) {
-  AT_CurrentSelector = selector;
-  AT_Config = config;
-  AT_CurrentSelector.append(
+  selector.append(
     `
       <div class="at-wrapper">
 
         <div class="at-header">
           <div class="at-view-count">
-            Show: <input placeholder="Interger" id="at-view-count" value="10"  type="text" onkeypress="return event.charCode >= 48 && event.charCode <= 57" /> results / 1 page
+            Show: <input placeholder="Interger" id="at-view-count" 
+              value="10" 
+              type="text" 
+              onkeypress="return event.charCode >= 48 && event.charCode <= 57" /> results / 1 page
           </div>
 
           <div class="at-search">
@@ -79,143 +113,173 @@ function AT_Init(selector, config) {
     `
   );
 
-  AT_RenderHeader();
-
-  AT_GoToPage(1);
-
-  AT_HandleShowCountEnter();
-  AT_HandleSearchInput();
-
-}
-
-function AT_HandleShowCountEnter() {
-  AT_CurrentSelector.find('#at-view-count').keyup(function HandleShowCountEnter(e) {
-    if (e.which === 13) {
-      AT_IsChangedShowCount = true;
-      AT_CurrentShowCount = $('#at-view-count').val().trim();
-      AT_GoToPage(1);
-    }    
+  selector.click(function AT_SelectorClick(e) {
+    AT_CurrentSelector = selector;
+    AT_Config = config;
   });
+
+  AT_RenderHeader(selector, config);
+
+  AT_GoToPage(1, selector, config);
+
+  AT_HandleShowCountEnter(selector);
+  AT_HandleSearchInput(selector);
+
 }
 
-function AT_HandleSearchInput() {
-  AT_CurrentSelector.find('#at-search-input').keyup(function HandleShowCountEnter(e) {
-    if (e.which === 13) {
-      AT_IsSearching = true;
-      AT_CurrentSearchInput = $('#at-search-input').val().trim();
-      AT_GoToPage(1);
-    }    
-  });
-}
+function AT_RenderHeader(selector, config) {
+  var headerHtml = '';
 
-function AT_RenderHeader() {
-  var headerHtml;
-
-  if (AT_Config.index) {
+  if (config.index) {
     headerHtml = '<div>Index</div>';
   }
 
-  for (var i = 0; i <= AT_Config.columns.length - 1; i++) {
-    headerHtml += '<div>' + AT_Config.columns[i].title + '</div>';
+  var id = selector.attr('id') ? selector.attr('id') : '';
+
+  for (var i = 0; i <= config.columns.length - 1; i++) {
+    if (config.columns[i].sortable) {
+      headerHtml += '<div class="at-sortable" onclick="AT_SortCLick(\'' + id + '\', \'' 
+        + config.columns[i].property + '\')">' 
+        + config.columns[i].title + '<img src=\'assets/sort.svg\'></div>';
+    } else {
+      headerHtml += '<div>' + config.columns[i].title + '</div>';
+    }
   }
 
-  AT_CurrentSelector.find('.at-body > .at-body-header').html(headerHtml);
+  selector.find('.at-body > .at-body-header').html(headerHtml);
 }
 
-function AT_GoToPage(page) {
-  AT_ToggleShowLoading();
-
-  var data = AT_Config.api.data;
-
-  data[AT_Config.api.pageIndexPath]   = page;
-  if (AT_IsChangedShowCount) {
-    data[AT_Config.api.viewCountPath] = AT_CurrentShowCount;
+function AT_SortCLick(id, property) {
+  var oldSort = $('#' + id).attr('data-at-sort', property);
+  if (oldSort !== undefined && oldSort !== null && oldSort !== '') {
+    if () {
+      
+    }
   } else {
-    data[AT_Config.api.viewCountPath] = 10;
+
   }
+}
 
-  data[AT_Config.api.pageIndexPath]   = page;
-  if (AT_IsSearching) {
-    data[AT_Config.api.searchInputPath] = AT_CurrentSearchInput;
-  } else {
-    data[AT_Config.api.searchInputPath] = '';
-  }
+function AT_HandleShowCountEnter(selector) {
+  selector.find('#at-view-count').keyup(function HandleShowCountEnter(e) {
+    if (e.which === 13) {
+      selector.attr('data-at-is-changed-show-count', 'true');
+      selector.attr('data-at-current-show-count', selector.find('#at-view-count').val().trim());
+      AT_GoToPage(1);
+    }    
+  });
+}
 
-  $.ajax({
-    url     : AT_Config.api.url,
-    type    : AT_Config.api.method,
-    dataType: AT_Config.api.dataType,
-    data    : AT_Config.api.dataType === 'GET' ? {} : data,
-    headers : AT_Config.api.headers,
-    success : (response) => {
-      if (response[AT_Config.api.statusPath]) {
+function AT_HandleSearchInput(selector) {
+  selector.find('#at-search-input').keyup(function HandleShowCountEnter(e) {
+    if (e.which === 13) {
+      selector.attr('data-at-is-searching', 'true');
+      selector.attr('data-at-current-search-input', selector.find('#at-search-input').val().trim());
+      AT_GoToPage(1);
+    }    
+  });
+}
 
-        AT_RenderTableBody(response, page);
-        AT_RenderFooter(response, page);
+function AT_GoToPage(page, selector, config) {
+  setTimeout(() => {
+    config = config ? config : AT_Config;
+    selector = selector ? selector : AT_CurrentSelector;
+
+    AT_ShowLoading(selector);
+
+    var data = config.api.data;
+
+    data[config.api.pageIndexPath]   = page;
+    if (selector.attr('data-at-is-changed-show-count') === 'true') {
+      data[config.api.viewCountPath] = selector.attr('data-at-current-show-count');
+    } else {
+      data[config.api.viewCountPath] = 10;
+    }
+
+    data[config.api.pageIndexPath]   = page;
+    if (selector.attr('data-at-is-searching') === 'true') {
+      data[config.api.searchInputPath] = selector.attr('data-at-current-search-input');
+    } else {
+      data[config.api.searchInputPath] = '';
+    }
+
+    $.ajax({
+      url     : config.api.url,
+      type    : config.api.method,
+      dataType: config.api.dataType,
+      data    : config.api.dataType === 'GET' ? {} : data,
+      headers : config.api.headers,
+      success : (response) => {
+        if (response[config.api.statusPath]) {
+
+          AT_RenderTableBody(response, page, selector, config);
+          AT_RenderFooter(response, page, selector, config);
+          setTimeout(() => {
+            AT_HideLoading(selector);
+          }, 100);
+
+        } else {
+          setTimeout(() => {
+            AT_HideLoading(selector);
+          }, 100);
+          console.log('[Ajax Table] Call api error: ', response);
+        }
+      },
+      error   : (error) => {
         setTimeout(() => {
-          AT_ToggleShowLoading();
-        }, 100);
-      } else {
-        setTimeout(() => {
-          AT_ToggleShowLoading();
+          AT_HideLoading(selector);
         }, 100);
         console.log('[Ajax Table] Call api error: ', response);
       }
-    },
-    error   : (error) => {
-      setTimeout(() => {
-        AT_ToggleShowLoading();
-      }, 100);
-      console.log('[Ajax Table] Call api error: ', response);
-    }
-  });
+    });
+  }, 1);
 
 }
 
-function AT_RenderTableBody(response, page) {
+function AT_RenderTableBody(response, page, selector, config) {
   var bodyHtml = '';
   var viewCount = $('#at-view-count').val().trim();
 
-  for (var i = 0; i <= response[AT_Config.api.dataPath].length - 1; i++) {
+  for (var i = 0; i <= response[config.api.dataPath].length - 1; i++) {
     var tdHtml = '<div class="at-row">';
-    if (AT_Config.index) {
+    if (config.index) {
       tdHtml += '<div>' + (((page - 1) * viewCount) + i + 1) + '</div>';
     }
-    for (var j = 0; j <= AT_Config.columns.length - 1; j++) {
-      tdHtml += '<div>' + response[AT_Config.api.dataPath][i][AT_Config.columns[j].property] + '</div>';
+    for (var j = 0; j <= config.columns.length - 1; j++) {
+      tdHtml += '<div>' + response[config.api.dataPath][i][config.columns[j].property] + '</div>';
     }
     tdHtml += '</div>';
 
     bodyHtml += tdHtml;
   }
 
-  AT_CurrentSelector.find('.at-body > .at-body-rows').html(bodyHtml);
+  selector.find('.at-body > .at-body-rows').html(bodyHtml);
 }
 
-function AT_RenderFooter(response, page) {
+function AT_RenderFooter(response, page, selector, config) {
   var viewCount = $('#at-view-count').val().trim();
   var footerLeftHtml = '<div>Showing from </div>';
 
-  var fromCount = response[AT_Config.api.dataPath].length > 0 
+  var fromCount = response[config.api.dataPath].length > 0 
     ? ((page - 1) * viewCount + 1) : 0;
 
-  var toCount = (page * viewCount) < response[AT_Config.api.countPath] 
-    ? (page * viewCount) : response[AT_Config.api.countPath];
+  var toCount = (page * viewCount) < response[config.api.countPath] 
+    ? (page * viewCount) : response[config.api.countPath];
 
   footerLeftHtml += '<div class="at-params">' + fromCount + '</div>';
   footerLeftHtml += '<div> to </div>';
   footerLeftHtml += '<div class="at-params">' + toCount + '</div>';
   footerLeftHtml += '<div> in total </div>';
-  footerLeftHtml += '<div class="at-params">' + (response[AT_Config.api.countPath]) + '</div>';
+  footerLeftHtml += '<div class="at-params">' + (response[config.api.countPath]) + '</div>';
   footerLeftHtml += '<div> results.</div>';
 
-  AT_CurrentSelector.find('.at-footer > .at-footer-left').html(footerLeftHtml);
+  selector.find('.at-footer > .at-footer-left').html(footerLeftHtml);
 
   var pageCount = 1;
-  if (response[AT_Config.api.countPath] % viewCount === 0) {
-    pageCount = Math.floor(response[AT_Config.api.countPath] / viewCount);
+  if (response[config.api.countPath] % viewCount === 0) {
+    pageCount = Math.floor(response[config.api.countPath] / viewCount);
    } else {
-    pageCount = Math.floor(response[AT_Config.api.countPath] / viewCount) + 1;
+    pageCount = Math.floor(response[config.api.countPath] / viewCount) + 1;
   }
 
   var footerRightHtml = 
@@ -236,35 +300,44 @@ function AT_RenderFooter(response, page) {
     `
   ;
 
-  AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper').html(footerRightHtml);
+  selector.find('.at-footer > .at-footer-right > .at-paging-wrapper').html(footerRightHtml);
 
   if (page === 1) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-first').hide();
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-pre').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-first').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-pre').hide();
   }
   if (page < 2) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-pre-1').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-pre-1').hide();
   }
   if (page < 3) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-pre-2').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-pre-2').hide();
   }
   if (page === pageCount || pageCount === 0) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-last').hide();
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-next').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-last').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-next').hide();
   }
   if (page > pageCount - 1) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-next-1').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-next-1').hide();
   }
   if (page > pageCount - 2) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-next-2').hide();
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-curr-next-2').hide();
   }
 
-  if (response[AT_Config.api.countPath] < 1) {
-    AT_CurrentSelector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-current-page').hide();
+  if (response[config.api.countPath] < 1) {
+    selector.find('.at-footer > .at-footer-right > .at-paging-wrapper .at-current-page').hide();
+  }
+
+  if (config.complete) {
+    config.complete();
   }
 
 }
 
-function AT_ToggleShowLoading() {
-  AT_CurrentSelector.find('.at-loading').toggle();
+function AT_ShowLoading(selector) {
+  selector.find('.at-loading').removeClass('at-display-block');
+  selector.find('.at-loading').addClass('at-display-block');
+}
+
+function AT_HideLoading(selector) {
+  selector.find('.at-loading').removeClass('at-display-block');
 }
